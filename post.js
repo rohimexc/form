1f98c5bbd10fa7a2f5f023e17f426aaa64e6532a
Abdul Rohim <101235723+rohimexc@users.noreply.github.com>
2024-04-06 02:27:52 +0800

    Update post.js

--- post.js
+++ post.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxfEjlufhsFr9z_UNmqnKZ83V6IJif6UrJrL2RwyUakgtwrM87hL6xwyzKP3DlKIVL5mw/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycby337o2QqYerofvtWQ8HMlGTL-pQxcICTXZtl8QuH0KIV3cwXBGtMdECS0wSGVkUl0D/exec';
 const form = document.forms['pendataan-mitra'];
 
 form.addEventListener('submit', e => {
